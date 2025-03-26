c17f7058fd7d721ec4079f9f9115dba7ae52da82
12101111 <w12101111@gmail.com>
2025-03-26 23:58:30 +0800

    Allow to disable initialize_all

--- src/module.rs
+++ src/module.rs
@@ -462,6 +462,7 @@ impl<'ctx> Module<'ctx> {
     /// ```
     // SubType: ExecutionEngine<Basic?>
     pub fn create_execution_engine(&self) -> Result<ExecutionEngine<'ctx>, LLVMString> {
+        #[cfg(not(feature = "disable-alltargets-init"))]
         Target::initialize_native(&InitializationConfig::default()).map_err(|mut err_string| {
             err_string.push('\0');
 
@@ -516,6 +517,7 @@ impl<'ctx> Module<'ctx> {
     /// ```
     // SubType: ExecutionEngine<Interpreter>
     pub fn create_interpreter_execution_engine(&self) -> Result<ExecutionEngine<'ctx>, LLVMString> {
+        #[cfg(not(feature = "disable-alltargets-init"))]
         Target::initialize_native(&InitializationConfig::default()).map_err(|mut err_string| {
             err_string.push('\0');
 
@@ -575,6 +577,7 @@ impl<'ctx> Module<'ctx> {
         &self,
         opt_level: OptimizationLevel,
     ) -> Result<ExecutionEngine<'ctx>, LLVMString> {
+        #[cfg(not(feature = "disable-alltargets-init"))]
         Target::initialize_native(&InitializationConfig::default()).map_err(|mut err_string| {
             err_string.push('\0');
 
@@ -659,6 +662,7 @@ impl<'ctx> Module<'ctx> {
         // ...
 
         // 1) Initialize the native target
+        #[cfg(not(feature = "disable-alltargets-init"))]
         Target::initialize_native(&InitializationConfig::default()).map_err(|mut err_string| {
             err_string.push('\0');
             LLVMString::create_from_str(&err_string)
